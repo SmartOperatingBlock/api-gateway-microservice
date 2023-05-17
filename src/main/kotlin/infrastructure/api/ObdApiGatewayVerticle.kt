1935292a63ec935017ba77edfd980cb15d920e00
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-05-17 09:51:19 +0200

    chore: create verticle for operating block dashboard api

---
+++ src/main/kotlin/infrastructure/api/ObdApiGatewayVerticle.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package infrastructure.api
+
+import infrastructure.api.handlers.AuthenticationHandler
+import infrastructure.provider.Provider
+import io.vertx.core.AbstractVerticle
+import io.vertx.ext.web.Router
+
+/**
+ * The Verticle for the Operating Block Dashboard Endpoint.
+ */
+class ObdApiGatewayVerticle(
+    private val provider: Provider,
+) : AbstractVerticle() {
+
+    private val router: Router = Router.router(this.vertx)
+
+    override fun start() {
+        router.post("$endpoint/auth").handler(AuthenticationHandler(provider))
+
+        vertx.createHttpServer()
+            .requestHandler(router)
+            .listen(port)
+    }
+
+    companion object {
+        private const val version = "v1"
+        private const val endpoint = "/api/$version/obd"
+        private const val port = 3002
+    }
+}
