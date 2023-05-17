8a4d6402e03e5557fe7c97beb7e1ef3e67eb2bad
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-05-17 18:36:07 +0200

    chore: implement request to get rooms

--- src/main/kotlin/infrastructure/webclient/WebClient.kt
+++ src/main/kotlin/infrastructure/webclient/WebClient.kt
@@ -7,17 +7,22 @@
  */
 
 package infrastructure.webclient
+import application.presenter.api.RoomDto
+import application.presenter.api.toRoom
 import application.presenter.api.toUserDto
-import entity.User
+import application.presenter.api.util.ApiResponses
+import entity.room.Room
+import entity.user.User
 import io.netty.handler.codec.http.HttpResponseStatus
 import io.vertx.core.Future
 import io.vertx.core.Vertx
-import io.vertx.core.json.Json
 import io.vertx.ext.web.client.WebClient
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import usecase.repository.AuthenticationRepository
-
+import usecase.repository.RoomRepository
 /** The Web Client responsible to make HTTP request to other microservices. */
-class WebClient(vertx: Vertx) : AuthenticationRepository {
+class WebClient(vertx: Vertx) : AuthenticationRepository, RoomRepository {
 
     init {
         MICROSERVICES_URL.forEach {
@@ -30,14 +35,25 @@ class WebClient(vertx: Vertx) : AuthenticationRepository {
     private val client: WebClient = WebClient.create(vertx)
 
     override fun getUserAuthentication(user: User): Future<Boolean> =
-        client.postAbs("$UMI_URI/auth").sendJson(Json.encode(user.toUserDto())).map {
+        client.postAbs("$UMI_URI/auth").sendJson(Json.encodeToString(user.toUserDto())).map {
             it.statusCode() == HttpResponseStatus.OK.code()
         }
 
+    override fun getRooms(): Future<List<Room>> =
+        client.getAbs("$BM_URI/rooms").send().map {
+            Json.decodeFromString<ApiResponses.ResponseEntryList<ApiResponses.ResponseEntry<RoomDto>>>(
+                it.bodyAsString(),
+            ).entries.map { responseEntry ->
+                responseEntry.entry.toRoom()
+            }
+        }
+
     companion object {
-        private val MICROSERVICES_URL = listOf<String?>(
-            System.getenv("USER_MANAGEMENT_MICROSERVICE_URL"),
-        )
         private val UMI_URI = System.getenv("USER_MANAGEMENT_MICROSERVICE_URL")
+        private val BM_URI = System.getenv("BUILDING_MANAGEMENT_MICROSERVICE_URL")
+        private val MICROSERVICES_URL = listOf<String?>(
+            UMI_URI,
+            BM_URI,
+        )
     }
 }
