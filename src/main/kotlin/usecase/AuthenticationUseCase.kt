5d7791c20cadeaf939adacdf76bf97287fca9520
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-05-17 15:06:43 +0200

    feat: create AuthenticationUseCase

---
+++ src/main/kotlin/usecase/AuthenticationUseCase.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package usecase
+
+import entity.User
+import io.vertx.core.Future
+import usecase.repository.AuthenticationRepository
+
+/**
+ * Class that models authentication use case.
+ */
+class AuthenticationUseCase(
+    private val user: User,
+    private val repository: AuthenticationRepository,
+) : UseCase<Future<Boolean>> {
+
+    override fun execute(): Future<Boolean> = repository.getUserAuthentication(user)
+}
