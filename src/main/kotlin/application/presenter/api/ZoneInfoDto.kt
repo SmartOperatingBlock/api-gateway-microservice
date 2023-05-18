574d3d1d2c98ffcbfd2fd7e9e9472eb16dfda7b1
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-05-18 13:48:33 +0200

    chore: create dto for zone information

---
+++ src/main/kotlin/application/presenter/api/ZoneInfoDto.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.api
+
+import kotlinx.serialization.Serializable
+
+/**
+ * The Dto for the information about a Zone of the Smart Operating Block.
+ * It includes the [zoneId], the [preOperatingRoomId] and the [operatingRoomId].
+ */
+@Serializable
+data class ZoneInfoDto(
+    val zoneId: String,
+    val preOperatingRoomId: String?,
+    val operatingRoomId: String?,
+)
