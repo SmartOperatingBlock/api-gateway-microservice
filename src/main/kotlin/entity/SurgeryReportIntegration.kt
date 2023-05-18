b150ee77ded25860a1eb19a7e0996aa6d7dbfb3f
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-05-18 15:05:22 +0200

    feat: create surgery report integration entity

---
+++ src/main/kotlin/entity/SurgeryReportIntegration.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity
+
+/**
+ * Models the surgery report integration.
+ * @param reportIntegration the integration of the report.
+ */
+data class SurgeryReportIntegration(
+    val reportIntegration: ReportIntegration,
+)
+
+/**
+ * The integration of the report.
+ * @param text the text of the integration.
+ */
+data class ReportIntegration(
+    val text: String,
+)
