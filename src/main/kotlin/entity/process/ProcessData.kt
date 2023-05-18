2e2f0ecc84d78a9cc64975ca84054a13683000ca
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-05-18 17:29:26 +0200

    chore: add surgical process data

---
+++ src/main/kotlin/entity/process/ProcessData.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.process
+
+import entity.room.RoomData
+import kotlinx.serialization.Serializable
+
+/**
+ * Module with all data for surgical processes.
+ */
+object ProcessData {
+
+    /**
+     * The [id] of the Surgical Process.
+     */
+    data class ProcessId(val id: String) {
+        init {
+            require(id.isNotEmpty()) {
+                "Process id cannot be empty!"
+            }
+        }
+    }
+
+    /**
+     * The [id] of the Patient.
+     */
+    data class PatientId(val id: String) {
+        init {
+            require(id.isNotEmpty()) {
+                "Patient id cannot be empty!"
+            }
+        }
+    }
+
+    /**
+     * The [id] of the Health Professional.
+     */
+    data class HealthProfessionalId(val id: String) {
+        init {
+            require(id.isNotEmpty()) {
+                "Health Professional id cannot be empty!"
+            }
+        }
+    }
+
+    /**
+     * The type of room inside the Operating Block composed by [id] and [type].
+     */
+    @Serializable
+    data class RoomWithType(
+        val id: String,
+        val type: RoomData.RoomType,
+    )
+
+    /** The different states of a [SurgicalProcess]. */
+    enum class ProcessState {
+        PRE_SURGERY, SURGERY, POST_SURGERY, INTERRUPTED, TERMINATED
+    }
+
+    /** The different steps of a [SurgicalProcess]. */
+    enum class ProcessStep {
+        PATIENT_IN_PREPARATION,
+        PATIENT_ON_OPERATING_TABLE,
+        ANESTHESIA,
+        SURGERY_IN_PROGRESS,
+        END_OF_SURGERY,
+        PATIENT_UNDER_OBSERVATION,
+    }
+}
