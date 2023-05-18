ba1099a8135d494cd1f4f244b006cd88605fd0f4
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-05-18 15:07:00 +0200

    feat: create use case class for surgery report integration

---
+++ src/main/kotlin/usecase/SurgeryReportIntegrationUseCase.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package usecase
+
+import entity.SurgeryReportIntegration
+import io.vertx.core.Future
+import usecase.repository.SurgeryReportIntegrationRepository
+
+/**
+ * Class that models surgery report integration use case.
+ */
+class SurgeryReportIntegrationUseCase(
+    private val integration: SurgeryReportIntegration,
+    private val repository: SurgeryReportIntegrationRepository,
+) : UseCase<Future<Boolean>> {
+
+    override fun execute(): Future<Boolean> = repository.integrateReport(integration)
+}
