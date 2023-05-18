2814faea1f1511f93c8ae550ae4da23d17bea613
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-05-18 15:05:34 +0200

    feat: create surgery report integration dto

---
+++ src/main/kotlin/application/presenter/api/SurgeryReportIntegrationDto.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.api
+
+import entity.ReportIntegration
+import entity.SurgeryReportIntegration
+import kotlinx.serialization.Serializable
+
+/**
+ * The Dto for surgery report integration.
+ * @param reportIntegration the integration of the report.
+ */
+@Serializable
+data class SurgeryReportIntegrationDto(
+    val reportIntegration: String,
+)
+
+/**
+ * Extension function used to convert [SurgeryReportIntegrationDto] to [SurgeryReportIntegration].
+ */
+fun SurgeryReportIntegrationDto.toSurgeryReportIntegration(): SurgeryReportIntegration =
+    SurgeryReportIntegration(
+        ReportIntegration(this.reportIntegration),
+    )
+
+/**
+ * Extension function used to convert [SurgeryReportIntegration] to [SurgeryReportIntegrationDto].
+ */
+fun SurgeryReportIntegration.toSurgeryReportIngrationDto(): SurgeryReportIntegrationDto =
+    SurgeryReportIntegrationDto(
+        this.reportIntegration.text,
+    )
