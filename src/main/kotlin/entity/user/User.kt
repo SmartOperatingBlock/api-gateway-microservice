ec0773a525df128f34da5fd0ba9a2cb05270111e
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-05-18 14:53:48 +0200

    chore: add use of value object in user entity

--- src/main/kotlin/entity/user/User.kt
+++ src/main/kotlin/entity/user/User.kt
@@ -5,13 +5,30 @@
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  */
-
 package entity.user
 
 /**
  * The user for the auth api composed by the [userId] and [password].
  */
 data class User(
-    val userId: String,
-    val password: String,
+    val userId: UserId,
+    val password: Password,
 )
+
+/**
+ * The [id] of the user.
+ */
+data class UserId(val id: String) {
+    init {
+        require(id.isNotEmpty())
+    }
+}
+
+/**
+ * The [password] of the user.
+ */
+data class Password(val password: String) {
+    init {
+        require(password.isNotEmpty())
+    }
+}
