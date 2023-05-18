4462b204e5348b782c9352cfd28936fda071e1d1
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-05-18 15:06:14 +0200

    feat: create interface for surgery report integration

---
+++ src/main/kotlin/usecase/repository/SurgeryReportIntegrationRepository.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package usecase.repository
+
+import entity.SurgeryReportIntegration
+import io.vertx.core.Future
+
+/**
+ * Repository for the surgery report integration.
+ */
+interface SurgeryReportIntegrationRepository {
+
+    /**
+     * Integrate the surgery report.
+     * @param surgeryReportIntegration the integration of the surgery report.
+     */
+    fun integrateReport(surgeryReportIntegration: SurgeryReportIntegration): Future<Boolean>
+}
