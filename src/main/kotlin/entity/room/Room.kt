96f937c6d78694e91c246f341d9b9c79efbb99b4
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-05-18 16:04:49 +0200

    chore: create dto for room data

--- src/main/kotlin/entity/room/Room.kt
+++ src/main/kotlin/entity/room/Room.kt
@@ -8,13 +8,24 @@
 
 package entity.room
 
+import entity.environment.RoomEnvironmentalData
+
 /**
  * The model of the Room entity composed by [id], [name],
- * [zoneId] and the [type].
+ * [zoneId], the [type] and the [environmentalData].
  */
 data class Room(
     val id: RoomData.RoomId,
-    val name: String,
+    val name: String?,
     val zoneId: RoomData.ZoneId,
     val type: RoomData.RoomType,
-)
+    val environmentalData: RoomEnvironmentalData = RoomEnvironmentalData(),
+) {
+    override fun equals(other: Any?): Boolean = when {
+        other === this -> true
+        other is Room -> this.id == other.id
+        else -> false
+    }
+
+    override fun hashCode(): Int = this.id.hashCode()
+}
