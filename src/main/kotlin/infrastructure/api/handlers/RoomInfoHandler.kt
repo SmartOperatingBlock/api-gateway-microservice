fa21d6e3e85aaa0d1fcf5a92516016ec5dc80985
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-05-18 16:07:13 +0200

    chore: create handler for room info api

---
+++ src/main/kotlin/infrastructure/api/handlers/RoomInfoHandler.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package infrastructure.api.handlers
+
+import application.presenter.api.RoomSerialization.toRoomDto
+import application.service.RoomService
+import entity.room.RoomData
+import infrastructure.provider.Provider
+import io.netty.handler.codec.http.HttpResponseStatus
+import io.vertx.core.Handler
+import io.vertx.ext.web.RoutingContext
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+/**
+ * The Handler for the information about a Room API.
+ */
+class RoomInfoHandler(
+    private val provider: Provider,
+) : Handler<RoutingContext> {
+
+    override fun handle(routingContext: RoutingContext) {
+        routingContext.pathParam("roomId")?.let {
+            RoomService.GetRoomEnvironmentalInfo(
+                RoomData.RoomId(it),
+                provider.webClient,
+            ).execute().onSuccess { room ->
+                routingContext.response()
+                    .setStatusCode(HttpResponseStatus.OK.code())
+                    .end(
+                        Json.encodeToString(room.toRoomDto()),
+                    )
+            }
+        }
+    }
+}
