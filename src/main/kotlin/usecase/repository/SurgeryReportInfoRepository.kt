792077040bb75decf3cca2be638ec1f723bbe345
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-05-18 15:04:29 +0200

    chore: create interface for surgery report info repository

---
+++ src/main/kotlin/usecase/repository/SurgeryReportInfoRepository.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package usecase.repository
+
+import entity.SurgeryReportInfo
+import io.vertx.core.Future
+
+/**
+ * Repository for surgery report info.
+ */
+interface SurgeryReportInfoRepository {
+
+    /**
+     * get the surgery report info given the [processId].
+     */
+    fun getSurgeryReportInfo(processId: String): Future<SurgeryReportInfo>
+}
