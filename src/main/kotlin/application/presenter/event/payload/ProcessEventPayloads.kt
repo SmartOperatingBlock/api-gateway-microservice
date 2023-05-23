9f8d44d68519e8942814cf14357890b5e33a7331
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-05-23 11:47:40 +0200

    chore: create payload for custom light request event

--- src/main/kotlin/application/presenter/event/payload/ProcessEventPayloads.kt
+++ src/main/kotlin/application/presenter/event/payload/ProcessEventPayloads.kt
@@ -9,6 +9,7 @@
 package application.presenter.event.payload
 
 import application.presenter.event.ProcessEvent
+import kotlinx.serialization.Serializable
 
 /** Module that wraps all payloads of process events. */
 object ProcessEventPayloads {
@@ -19,5 +20,16 @@ object ProcessEventPayloads {
     /**
      * The payload of a process manual event with the [roomId] and the [manualEvent].
      */
+    @Serializable
     data class ProcessManualEvent(val roomId: String, val manualEvent: String) : ProcessEventPayload
+
+    /**
+     * The payload of custom light request event composed by the [roomId], [ambientLightLux] and [surgicalLightLux].
+     */
+    @Serializable
+    data class CustomLightRequestEvent(
+        val roomId: String,
+        val ambientLightLux: Int,
+        val surgicalLightLux: Int,
+    ) : ProcessEventPayload
 }
