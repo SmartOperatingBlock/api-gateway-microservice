ed32b24d8fdf0aae27dfe11857ee6e0351e21635
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-05-23 11:57:15 +0200

    chore: add stop custom scenario api endpoint to ord gateway

--- src/main/kotlin/infrastructure/api/OrdApiGatewayVerticle.kt
+++ src/main/kotlin/infrastructure/api/OrdApiGatewayVerticle.kt
@@ -12,6 +12,7 @@ import infrastructure.api.handlers.CustomScenarioHandler
 import infrastructure.api.handlers.ProcessManualEventHandler
 import infrastructure.api.handlers.ProcessStateHandler
 import infrastructure.api.handlers.RoomInfoHandler
+import infrastructure.api.handlers.StopCustomScenarioHandler
 import infrastructure.provider.Provider
 import io.vertx.core.AbstractVerticle
 import io.vertx.ext.web.Router
@@ -30,6 +31,7 @@ class OrdApiGatewayVerticle(
         router.get("$endpoint/process-state").handler(ProcessStateHandler(provider))
         router.post("$endpoint/process-manual-events").handler(ProcessManualEventHandler(this.vertx))
         router.post("$endpoint/custom-automation-scenario").handler(CustomScenarioHandler(this.vertx))
+        router.post("$endpoint/stop-custom-automation-scenario").handler(StopCustomScenarioHandler(this.vertx))
         vertx.createHttpServer()
             .requestHandler(router)
             .listen(port)
