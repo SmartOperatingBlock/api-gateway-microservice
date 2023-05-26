3f4c1bdbaeaa531902513f825482d107be740ade
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-05-26 17:27:53 +0200

    chore: add method to obtain the Room from the dto

--- src/main/kotlin/application/presenter/serialization/RoomSerializer.kt
+++ src/main/kotlin/application/presenter/serialization/RoomSerializer.kt
@@ -12,8 +12,14 @@ import application.presenter.api.report.measurements.ValueWithUnit
 import application.presenter.api.report.room.RoomApiDto
 import application.presenter.api.report.room.RoomApiDtoType
 import application.presenter.api.report.room.RoomEnvironmentalDataApiDto
+import entity.surgeryreport.measurements.Humidity
+import entity.surgeryreport.measurements.Luminosity
+import entity.surgeryreport.measurements.Percentage
+import entity.surgeryreport.measurements.Presence
+import entity.surgeryreport.measurements.Temperature
 import entity.surgeryreport.room.Room
 import entity.surgeryreport.room.RoomEnvironmentalData
+import entity.surgeryreport.room.RoomID
 import entity.surgeryreport.room.RoomType
 
 /**
@@ -29,11 +35,27 @@ object RoomSerializer {
     )
 
     /**
+     * Extension method to obtain the room from the dto.
+     */
+    fun RoomApiDto.toRoom(): Room = Room(
+        RoomID(this.id),
+        this.type.toRoomType(),
+    )
+
+    /**
      * Extension method to obtain the api dto type of a room.
      */
     fun RoomType.toApiDtoType() = when (this) {
         RoomType.OPERATING_ROOM -> RoomApiDtoType.OPERATING_ROOM
         RoomType.PRE_OPERATING_ROOM -> RoomApiDtoType.PRE_OPERATING_ROOM
+    }
+
+    /**
+     * Extension method to obtain the room type from the api dto.
+     */
+    fun RoomApiDtoType.toRoomType() = when (this) {
+        RoomApiDtoType.OPERATING_ROOM -> RoomType.OPERATING_ROOM
+        RoomApiDtoType.PRE_OPERATING_ROOM -> RoomType.PRE_OPERATING_ROOM
     }
 
     /**
@@ -44,5 +66,15 @@ object RoomSerializer {
         humidity = this.humidity?.percentage?.value,
         luminosity = this.luminosity?.let { ValueWithUnit(it.value, it.unit.toString()) },
         presence = this.presence?.presenceDetected,
+    )
+
+    /**
+     * Extension method to obtain room environmental data from dto.
+     */
+    fun RoomEnvironmentalDataApiDto.toRoomEnvironmentalData() = RoomEnvironmentalData(
+        temperature = this.temperature?.let { Temperature(it.value) },
+        humidity = this.humidity?.let { Percentage(it) }?.let { Humidity(it) },
+        luminosity = this.luminosity?.let { Luminosity(it.value) },
+        presence = this.presence?.let { Presence(it) },
     )
 }
